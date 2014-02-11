5f6b95be4b801ce1e921c5439b5191cd2e0fea1d
Ivo Smits <Ivo@UCIS.nl>
2014-02-11 17:45:33 +0100

    NaCl: fixed MAC/hash verification code

--- NaCl/crypto_verify/16.cs
+++ NaCl/crypto_verify/16.cs
@@ -5,7 +5,7 @@ namespace UCIS.NaCl.crypto_verify {
 		const int BYTES = 16;
 		public static int crypto_verify(Byte* x, Byte* y) {
 			Int32 differentbits = 0;
-			for (int i = 0; i < 15; i++) differentbits |= x[i] ^ y[i];
+			for (int i = 0; i < 16; i++) differentbits |= x[i] ^ y[i];
 			return (1 & ((differentbits - 1) >> 8)) - 1;
 		}
 	}
--- NaCl/crypto_verify/32.cs
+++ NaCl/crypto_verify/32.cs
@@ -5,7 +5,7 @@ namespace UCIS.NaCl.crypto_verify {
 		const int BYTES = 32;
 		public static int crypto_verify(Byte* x, Byte* y) {
 			Int32 differentbits = 0;
-			for (int i = 0; i < 31; i++) differentbits |= x[i] ^ y[i];
+			for (int i = 0; i < 32; i++) differentbits |= x[i] ^ y[i];
 			return (1 & ((differentbits - 1) >> 8)) - 1;
 		}
 	}
